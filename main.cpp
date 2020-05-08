62b9454842e7f791255229e8b479f9b98edba552
WillyValle <64986868+WillyValle@users.noreply.github.com>
2020-05-08 14:24:55 -0600

    Revert "Update main.cpp"
    This reverts commit 8086de553b0dcd48249b955f881ca590fa672065.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,6 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char** argv) {
-	cout<<"hola mundo"<<n1<<n2<<endl;
+	cout<<"hola mundo"<<n1<<endl;
 	return 0;
 }
